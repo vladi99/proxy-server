cff4f2a462eb699bda22a3cf318ff880befbf5bc
vladi99 <vlpetrovic99@gmail.com>
2020-05-06 21:55:42 +0100

    Fix proxy

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
 
-const proxy = httpProxy.createProxyServer({});
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
+const target = process.env.TARGET;
+
+const proxy = httpProxy.createProxyServer({
+  target,
+  changeOrigin: true
+});
 
 http.createServer( (req, res) => {
-  res.write('Hello World!');
-  res.end();
-  // proxy.web(req, res, { target: process.env.URL });
+  proxy.web(req, res);
 }).listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
