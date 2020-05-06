a025a70de58d5be57552e4954a2bf859f6b57267
vladi99 <vlpetrovic99@gmail.com>
2020-05-06 15:25:38 +0100

    Add logs
    Test

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 
 const proxy = httpProxy.createProxyServer({});
-
-const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: process.env.URL });
-});
-
 const PORT = process.env.PORT || 8000
 
-server.listen(() => {
+http.createServer( (req, res) => {
+  res.write('Hello World!');
+  res.end();
+  // proxy.web(req, res, { target: process.env.URL });
+}).listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
